94677d4460052058f4d9c0d1740b2a144c71c73c
xiu619544553 <619544553@qq.com>
2015-07-20 14:53:39 +0800

    今天做了15个文件夹的啊啊啊啊。好累。

---
+++ GIT/GIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 韩秀辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT/GIT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GIT
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 韩秀辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
